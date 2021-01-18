84508ba92fb1c05eb0c150d5f5ab4e5a732adf69
Naman Gupta <namangupta.cs@gmail.com>
2021-01-18 19:58:44 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import pickle
+from keras.models import model_from_json
+import pandas as pd
+import datetime
+import re
+
+from final_file import final
+import flask
+app = Flask(__name__)
+
+final_object=final()
+
+
+@app.route('/',methods=['GET'])
+def home():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	features = [x for x in request.form.to_dict()]
+    //index=request.form.to_dict()['Enter_index']
+    //date=request.form.to_dict()['Enter_date']
+    client,access,language,predicted,time=final_object.predict(features[0],features[1])
+    return flask.render_template('new.html',Client=client,Access=access,Language=language,predicted=predicted,time=time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
