2aaef3c4d89887931d66ddcfa3b33f5b6d64373d
Naman Gupta <namangupta.cs@gmail.com>
2021-01-19 22:49:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     features = [x for x in request.form.values()]
-    client,access,language,predicted,time=final_object.predict(features[0],features[1])
-    return flask.render_template('new.html',Client=client,Access=access,Language=language,predicted=predicted,time=time)
+    client,access,language,predicted,time,page=final_object.predict(features[0],features[1])
+    return flask.render_template('new.html',Client=client,Access=access,Language=language,predicted=predicted,time=time,Page=page)
 
 if __name__ == '__main__':
     app.run(debug=True)
